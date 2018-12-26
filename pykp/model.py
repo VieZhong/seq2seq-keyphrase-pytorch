976233ae6937845cbe1cce54c1531d986acd3a0f
VieZhong <zyw13579@qq.com>
2018-12-26 11:03:02 +0800

    fixed

--- pykp/model.py
+++ pykp/model.py
@@ -277,7 +277,7 @@ class Seq2SeqLSTMAttention(nn.Module):
             self.vocab_size,
             self.emb_dim,
             self.pad_token_src
-        )
+        ).cuda()
 
         self.encoder = nn.LSTM(
             input_size=self.emb_dim,
@@ -413,7 +413,7 @@ class Seq2SeqLSTMAttention(nn.Module):
         self.h0_encoder, self.c0_encoder = self.init_encoder_state(input_src)  # (self.encoder.num_layers * self.num_directions, batch_size, self.src_hidden_dim)
 
         # input (batch_size, src_len), src_emb (batch_size, src_len, emb_dim)
-        src_emb = self.embedding(input_src).cuda()
+        src_emb = self.embedding(input_src)
         src_emb = nn.utils.rnn.pack_padded_sequence(src_emb, input_src_len, batch_first=True)
 
         # src_h (batch_size, seq_len, hidden_size * num_directions): outputs (h_t) of all the time steps
